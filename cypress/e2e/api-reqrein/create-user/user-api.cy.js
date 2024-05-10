d6f610e9f6bedf6ebc45f56bf97e20a7dbddc4db
tobiascamilo <tobiascamylo@gmail.com>
2024-05-10 13:57:31 -0300

    Ajuste da identação do codigo fonte do projeto

--- cypress/e2e/api-reqrein/create-user/user-api.cy.js
+++ cypress/e2e/api-reqrein/create-user/user-api.cy.js
@@ -1,6 +1,6 @@
 function generateUser() {
-  const names = ['John', 'Jane', 'Sam', 'Sue'];
-  const jobs = ['developer', 'tester', 'manager', 'analyst'];
+  const names = ["John", "Jane", "Sam", "Sue"];
+  const jobs = ["developer", "tester", "manager", "analyst"];
 
   const name = names[Math.floor(Math.random() * names.length)];
   const job = jobs[Math.floor(Math.random() * jobs.length)];
@@ -8,39 +8,39 @@ function generateUser() {
   return { name, job };
 }
 
-describe('User API Tests', () => {
-  it('Create User', () => {
+describe("User API Tests", () => {
+  it("Create User", () => {
     const user = generateUser();
     cy.createUser(user).then((response) => {
       expect(response.status).to.eq(201);
-      expect(response.body).to.have.property('name', user.name);
-      expect(response.body).to.have.property('job', user.job);
-      expect(response.body).to.have.property('id');
-      expect(response.body).to.have.property('createdAt');
+      expect(response.body).to.have.property("name", user.name);
+      expect(response.body).to.have.property("job", user.job);
+      expect(response.body).to.have.property("id");
+      expect(response.body).to.have.property("createdAt");
     });
   });
 
-  it('Create User without name', () => {
-    const user = { job: 'leader' };
+  it("Create User without name", () => {
+    const user = { job: "leader" };
     cy.createUser(user).then((response) => {
       expect(response.status).to.eq(201);
-      expect(response.body).to.have.property('job', user.job);
-      expect(response.body).to.have.property('id');
-      expect(response.body).to.have.property('createdAt');
+      expect(response.body).to.have.property("job", user.job);
+      expect(response.body).to.have.property("id");
+      expect(response.body).to.have.property("createdAt");
     });
   });
 
-  it('Create User without job', () => {
-    const user = { name: 'morpheus' };
+  it("Create User without job", () => {
+    const user = { name: "morpheus" };
     cy.createUser(user).then((response) => {
       expect(response.status).to.eq(201);
-      expect(response.body).to.have.property('name', user.name);
-      expect(response.body).to.have.property('id');
-      expect(response.body).to.have.property('createdAt');
+      expect(response.body).to.have.property("name", user.name);
+      expect(response.body).to.have.property("id");
+      expect(response.body).to.have.property("createdAt");
     });
   });
 
-  it('Create User with null fields', () => {
+  it("Create User with null fields", () => {
     const user = { name: null, job: null };
     cy.createUser(user).then((response) => {
       expect(response.status).to.eq(201);
